1bbce7ab000783640a624be21f702bdb94519b82
Kate <katshe18@gmail.com>
2024-01-05 17:35:33 +1300

    First go with React, used create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        { <h1>Kate: Hello World</h1>}
+       { <p>First go at React</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
